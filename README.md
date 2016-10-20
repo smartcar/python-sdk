33aa105833993351365579b7553c55af56bf715b
Zane Gregg Bradley <zanegbradley@gmail.com>
2016-10-20 15:16:16 -0700

    add gitignore, travis, readme, and setup.py

---
+++ README.md
@@ -0,0 +1,11 @@
+### Smartcar Python SDK
+
+Get started at https://developer.smartcar.com
+
+### Installation
+
+    pip install smartcar
+
+### Documentation
+
+TODO: documentation
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='smartcar',
+    version='0.0.1',
+    description='Smartcar Python SDK',
+    author='Smartcar',
+    packages=['smartcar'],
+    url='https://github.com/smartcar/python-sdk',
+    license='MIT',
+    install_requires=[
+        'requests'
+    ]
+)
+        
+
