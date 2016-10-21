9aa0c6122423938f8ca6092c6f86b1ff5ee1dd68
Zane Gregg Bradley <zanegbradley@gmail.com>
2016-10-21 15:27:13 -0700

    add documentation to smartcar.py

--- smartcar/smartcar.py
+++ smartcar/smartcar.py
@@ -6,20 +6,49 @@ except ImportError:
     from urllib.parse import urlencode
 
 class Smartcar(object):
+    """
+    A client for accessing the Smartcar API
+
+    :param client_id: The application id, provided in the `application
+        dashboard`_
+
+    :param client_secret: The application secret, provided in the `application
+        dashboard`_
+
+    :param redirect_uri: The URL to redirect to after the user accepts
+        or declines the application's permissions. This URL must also be
+        present in the Redirect URIs field in the `application dashboard`_
+
+    :param scope: A list of permissions requested by the application
+
+    .. _application dashboard: https://developer.smartcar.com
+
+    """
     def __init__(self, client_id, client_secret, redirect_uri, scope):
         self.client_id = client_id
         self.client_secret = client_secret
         self.auth=(client_id, client_secret)
         self.redirect_uri = redirect_uri
         self.scope = scope
-    
-    def get_auth_url(self, oem, force=False, state=None):
-        """ Get an OEM authorization URL """
 
-        base_url = const.OEMS.get(oem) 
+    def get_auth_url(self, oem, force=False, state=None):
+        """
+        Generate an OAuth authentication URL for the specified OEM
+
+        :param str oem: The name of an oem
+
+        :param boolean force: Set to True in order to force the approval dialog
+            shown to the user
+
+        :param str state: A random string that will be passed back on redirect,
+            this allows protection against cross-site forgery requests
+
+        """
+
+        base_url = const.OEMS.get(oem)
         if not base_url:
             raise ValueError("specified oem is not supported")
-        
+
         approval_prompt = "force" if force else "auto"
         query = {
             "response_type": "code",
@@ -34,7 +63,12 @@ class Smartcar(object):
         return base_url + '/oauth/authorize?' + urlencode(query)
 
     def exchange_code(self, code):
-        """ Exchange an authentication code for an Access object """
+        """
+        Exchange an authentication code for an access object
+
+        :param code: A valid authorization code
+
+        """
         method = "POST"
         url = const.AUTH_URL
         params = {
@@ -43,10 +77,16 @@ class Smartcar(object):
             "redirect_uri": self.redirect_uri,
         }
         return requester.call(method, url, params=params, auth=self.auth)
-         
+
 
     def exchange_token(self, refresh_token):
-        """ Exchange a refresh token for a new Access object """
+        """
+        Exchange a refresh token for a new access object
+
+        :param refresh_token: A valid refresh token from a previously retrieved
+            access object
+
+        """
         method = "POST"
         url = const.AUTH_URL
         params = {
@@ -56,9 +96,27 @@ class Smartcar(object):
         return requester.call(method, url, params=params, auth=self.auth)
 
     def get_vehicles(self, access_token, limit=10, offset=0):
-        """ Get a list of the user's vehicles """
+        """
+        Get a list of the user's vehicles
+
+        :param access_token: A valid access token from a previously retrieved
+            access object
+
+        :param limit: The number of vehicles to return
+
+        :param offset: The index to start the vehicle list at
+
+        """
         return api.Api(access_token).vehicles(limit=limit, offset=offset)
 
     def get_vehicle(self, access_token, vehicle_id):
-        """ Get a Vehicle object representing a user's vehicle """
+        """
+        Get a Vehicle object representing a user's vehicle
+
+        :param access_token: A valid access token from a previously retrieved
+            access object
+
+        :param vehicle_id: A vehicle's id
+
+        """
         return vehicle.Vehicle(access_token, vehicle_id)
