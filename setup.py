808b33dfe2699cd14efd2ea0de211759608660ed
Jeremy Zhang <jeremy.zhang@smartcar.com>
2018-05-01 18:06:42 -0700

    delete requirements.txt

--- setup.py
+++ setup.py
@@ -4,17 +4,22 @@ with open('smartcar/__init__.py','r') as f:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
                         f.read(), re.MULTILINE).group(1)
 setup(
-    name="smartcar",
+    name='smartcar',
     version=version,
-    description="Smartcar Python SDK",
-    author="Zane Bradley",
-    author_email="zane@smartcar.com",
-    packages=["smartcar"],
-    url="https://github.com/smartcar/python-sdk",
-    license="MIT",
+    description='Smartcar Python SDK',
+    author='Smartcar',
+    author_email='hi@smartcar.com',
+    packages=['smartcar'],
+    url='https://github.com/smartcar/python-sdk',
+    license='MIT',
     install_requires=[
-        "requests"
+        'cookies',
+        'coverage',
+        'nose',
+        'mock',
+        'python-dateutil',
+        'requests',
+        'responses',
+        'six'
     ]
 )
-
-
