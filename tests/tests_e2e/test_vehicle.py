d07d050b64c8bf183781310c9b9b2885d2e00625
Karthik Bhaskara <karthikbhaskara96@gmail.com>
2019-06-25 14:29:13 -0700

    fix: Vehicle e2e tests to not depend on order

--- tests/tests_e2e/test_vehicle.py
+++ tests/tests_e2e/test_vehicle.py
@@ -2,11 +2,6 @@ import smartcar
 from test_base import TestBase
 import unittest
 
-# tests run in alphabetical order by default, however, we want
-# to run disconnect() last so we don't have to reauth again
-# the lambda function below sorts it in reverse alphabetical order
-unittest.TestLoader.sortTestMethodsUsing = lambda _, x, y: (y > x) - (y < x)
-
 
 class TestVehicleE2E(TestBase):
 
@@ -61,6 +56,21 @@ class TestVehicleE2E(TestBase):
     def test_unlock(self):
         unlock = self.vehicle.unlock()
         self.assertEqual(unlock["status"], "success")
+
+
+class TestVehicleDisconnectE2E(TestBase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestVehicleDisconnectE2E, cls).setUpClass()
+
+        access_object = cls.client.exchange_code(cls.code)
+
+        access_token = access_object['access_token']
+        vehicle_ids = smartcar.get_vehicle_ids(access_token)
+        cls.vehicle = smartcar.Vehicle(
+            vehicle_ids['vehicles'][0],
+            access_token)
 
     def test_disconnect(self):
         disconnected = self.vehicle.disconnect()
