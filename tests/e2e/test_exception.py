56561af158e40a19963230a2edea3e74cbc2ec91
Haig (Hike) Hovsepian <hhovsepi@gmail.com>
2024-04-23 10:05:05 -0500

    add suggested_user_message to SDK v2 err response

--- tests/e2e/test_exception.py
+++ tests/e2e/test_exception.py
@@ -163,3 +163,17 @@ def test_retry_after_found():
     except Exception as e:
         assert isinstance(e, SmartcarException)
         assert e.retry_after == 5000
+
+def test_suggested_user_message():
+    """
+    test that we can get the retry_after amount
+    """
+    try:
+        raise exception_factory(
+            429,
+            {"Retry-After": 5000, "Content-Type": "application/json"},
+            '{"statusCode":429,"type":"RATE_LIMIT","code":"Vehicle","resolution":{"type":"RETRY_LATER"},"requestId":"e0027f5f-4411-4247-a54d-e34c157d84c1", "suggestedUserMessage": "Please try again later."}',
+        )
+    except Exception as e:
+        assert isinstance(e, SmartcarException)
+        assert e.suggested_user_message == "Please try again later."
